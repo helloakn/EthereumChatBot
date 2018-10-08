3fd838cafee019b33def5ac3d697705a8963ab5f
akn <akn.social@gmail.com>
2018-10-08 13:51:02 +0630

    [modify] import django_heroku

--- BaseAPP/settings.py
+++ BaseAPP/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -117,8 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+print(STATIC_ROOT)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '/static_files/'),
 )
+django_heroku.settings(locals())
